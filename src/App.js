b8533d9f75d7e096bc17c6e63e8390c74f91374a
Sara Sipovac <sara.sipovac@limitmarkets.com>
2021-02-24 22:16:04 +0100

    make navbar

--- src/App.js
+++ src/App.js
@@ -11,9 +11,13 @@ function App() {
       <Layout.Header>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Početna</Menu.Item>
+          <Menu.Item key="2">O nama</Menu.Item>
+          <Menu.Item key="3">Prodaja</Menu.Item>
+          <Menu.Item key="4">Projekti</Menu.Item>
+          <Menu.Item key="5">Lokacija</Menu.Item>
+          <Menu.Item key="6">Kontakt</Menu.Item>
+          <Menu.Item key="7">Prijava</Menu.Item>{' '}
         </Menu>
       </Layout.Header>
       <Layout.Content style={{ height: '90%', border: '2px solid red' }}>
