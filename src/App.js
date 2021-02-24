e6d23a504cf3c36229a37f0cf593b6040d1e32d5
Sara Sipovac <sara.sipovac@limitmarkets.com>
2021-02-24 22:40:26 +0100

    set menu item's keys to be number

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
     >
       <Layout.Header>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">Početna</Menu.Item>
-          <Menu.Item key="2">O nama</Menu.Item>
-          <Menu.Item key="3">Prodaja</Menu.Item>
-          <Menu.Item key="4">Projekti</Menu.Item>
-          <Menu.Item key="5">Lokacija</Menu.Item>
-          <Menu.Item key="6">Kontakt</Menu.Item>
-          <Menu.Item key="7">Prijava</Menu.Item>{' '}
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key={1}>Početna</Menu.Item>
+          <Menu.Item key={2}>O nama</Menu.Item>
+          <Menu.Item key={3}>Prodaja</Menu.Item>
+          <Menu.Item key={4}>Projekti</Menu.Item>
+          <Menu.Item key={5}>Lokacija</Menu.Item>
+          <Menu.Item key={6}>Kontakt</Menu.Item>
+          <Menu.Item key={7}>Prijava</Menu.Item>
         </Menu>
       </Layout.Header>
       <Layout.Content style={{ height: '90%', border: '2px solid red' }}>
